3d84a68d4a7449ab1c63946e340499925a6de8a5
lprincewhn <lprincewhn@gmail.com>
2018-04-28 17:10:47 +0800

    1. Replace changeUser with change_prompt. 2. Add more unittest case.

--- unit.py
+++ unit.py
@@ -18,23 +18,69 @@ class TestSSHHandler(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def testNoPasswordRawActions(self):
+        prompt = '\[root@devtest ~\]# '
+        ssh = sshclient.SSHHandler(self.host, self.userwithoutpass, prompt)
+        end_action = sshclient.Action("end")
+        ls_action = sshclient.Action("command", "ls")
+        ls_action.add_next_action('\[vagrant@devtest ~\]\$', end_action)
+        su_action = sshclient.Action("command", "su - vagrant", False)
+        su_action.add_next_action('\[vagrant@devtest ~\]\$', ls_action)
+        result = ssh.start_action(su_action)
+        ssh.close()
+
+    def testPasswordRawActions(self):
+        prompt = '\[vagrant@devtest ~\]\$ '
+        ssh = sshclient.SSHHandler(self.host, self.userwithpass, prompt, self.userpass)
+        end_action = sshclient.Action("end")
+        ls_action = sshclient.Action("command", "ls")
+        ls_action.add_next_action('\[root@devtest ~\]#', end_action)
+        wpAction = sshclient.Action("exception")
+        wpAction.exception = sshclient.AuthenticationFailed()
+        passAction = sshclient.Action("command", "vagrant", False)
+        passAction.add_next_action('\[root@devtest ~\]#', ls_action)
+        passAction.add_next_action('Authentication failure', wpAction)
+        su_action = sshclient.Action("command", "su - ", False)
+        su_action.add_next_action('assword:', passAction)
+        result = ssh.start_action(su_action)
+        ssh.close()
+
+    def testPasswordChangePrompt(self):
+        prompt = '\[vagrant@devtest ~\]\$ '
+        ssh = sshclient.SSHHandler(self.host,  self.userwithpass, prompt, self.userpass)
+        ssh.change_prompt('su -', '\[root@devtest ~\]# ', 'vagrant')
+        ssh.close()
+
+    def testNoPasswordChangePrompt(self):
+        prompt = '\[root@devtest ~\]# '
+        ssh = sshclient.SSHHandler(self.host, self.userwithoutpass, prompt)
+        ssh.change_prompt('su - vagrant', '\[vagrant@devtest ~\]\$ ')
+        ssh.close()
+
     def testNoPasswordLogin(self):
         prompt = '\[root@devtest ~\]# '
         ssh = sshclient.SSHHandler(self.host, self.userwithoutpass, prompt)
-        result = ssh.runCommand('ls', self.outfile)
-        self.assertEqual(result, self.outfile.getvalue(), "Printout of runCommand is not equal to return value")
+        result = ssh.run_cmd('ls', self.outfile)
+        self.assertEqual(result, self.outfile.getvalue(), "Printout of run_cmd is not equal to return value")
         ssh.close()
 
     def testPasswordLogin(self):
         prompt = '\[vagrant@devtest ~\]\$ '
         ssh = sshclient.SSHHandler(self.host, self.userwithpass, prompt, self.userpass)
-        result = ssh.runCommand('ls', self.outfile)
-        self.assertEqual(result, self.outfile.getvalue(), "Printout of runCommand is not equal to return value")
+        result = ssh.run_cmd('ls', self.outfile)
+        self.assertEqual(result, self.outfile.getvalue(), "Printout of run_cmd is not equal to return value")
+        ssh.close()
+
+    def testLongOutput(self):
+        prompt = '\[root@devtest ~\]# '
+        ssh = sshclient.SSHHandler(self.host, self.userwithoutpass, prompt)
+        result = ssh.run_cmd("more /root/sshexpect/sshclient.py", self.outfile)
+        self.assertEqual(result, self.outfile.getvalue(), "Printout of run_cmd is not equal to return value")
         ssh.close()
 
     def testWrongHost(self):
         prompt = '\[vagrant@devtest ~\]\$ '
-        try: 
+        try:
             sshclient.SSHHandler('Nonexisithost', self.userwithpass, prompt, self.userpass)
         except sshclient.ConnectionError, e:
             pass
@@ -97,7 +143,7 @@ class TestSCPHandler(unittest.TestCase):
         try:
             scp.upload('./unit.py', '/home/vagrant/uploadedfile')
         except sshclient.AuthenticationFailed, e:
-            pass 
+            pass
 
     def testWrongUser(self):
         scp = sshclient.SCPHandler(self.host, 'vagrnt')
@@ -113,6 +159,7 @@ class TestSCPHandler(unittest.TestCase):
         except sshclient.SCPFailed, e:
             pass
 
+
+
 if __name__ == '__main__':
     unittest.main()
-
